a23333a59301d918a07540884dd5c21c72b7db66
William Assis <willifran@gmail.com>
2025-08-15 06:17:14 -0300

    adicionando a router da página index.html

--- router/clientes.js
+++ router/clientes.js
@@ -6,7 +6,7 @@ const Cliente = require('../models/Cliente');
 
 // Rota para inserir um novo cliente
 
-router.post('/cadastrar', (req, res) => {
+router.post('/cadastrar', (req, res) => { 
     const { nomeSobrenome, email, telefone, mensagem, contato, opcao, novidadeEmail } = req.body;
 
     const cliente = new Cliente(nomeSobrenome, email, telefone, mensagem, contato, opcao, novidadeEmail);
@@ -14,6 +14,11 @@ router.post('/cadastrar', (req, res) => {
     cliente.salvar()
         .then(() => res.send('Cliente cadastrado com sucesso!'))
         .catch(err => res.status(500).send('Erro ao cadastrar cliente: ' + err));
+});
+
+//Rota para minha página index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
